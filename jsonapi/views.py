7f1f9c20b133952f8670713f72161e4f4ce5641c
Sumit <tonystark83033@gmail.com>
2021-10-01 13:38:22 +0530

    Adding image1 image2 and random video url

--- jsonapi/views.py
+++ jsonapi/views.py
@@ -3,15 +3,31 @@ from django.shortcuts import render
 from django.urls.conf import path
 from django.shortcuts import render,HttpResponse
 from django.http import JsonResponse
+from random import randrange
+
+
+
 
 # Create your views here.
 
 def home(request):
+    a= randrange(6)
+    mylist =[
+        "https://www.youtube.com/watch?v=3-WHQzEO7KU",
+        "https://www.youtube.com/watch?v=N7Sx9S31Cnk",
+        "https://www.youtube.com/watch?v=FvmtcuBnD6M",
+        "https://www.youtube.com/watch?v=FvmtcuBnD6M",
+        "https://www.youtube.com/watch?v=FvmtcuBnD6M",
+        "https://www.youtube.com/watch?v=WTQlEvEKNFU",
+    
+    ]
+   
     mydata = {
-        "name": "sumit",
-        "class" : "12",
-        "age": "18"
+        "image1": "https://source.unsplash.com/480x1210/?nature,human",
+        "image2" : "https://source.unsplash.com/1070x201/?nature,water",
+        "video": mylist[a]
     }
+    
     return JsonResponse(mydata)
 
 
